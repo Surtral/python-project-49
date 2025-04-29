e76f65869716ab4da8888df56ceef2f9d97eae32
Vasiliy <dartanimal@bk.ru>
2025-04-29 16:53:17 +0300

    new game

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,25 @@
+import random
+
+def is_even(number):
+    return number % 2 == 0
+
+def main():
+    from brain_games.cli import welcome_user
+    name = welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    for i in range(3):
+        number = random.randint(1, 100)
+        print(f'Question: {number}')
+        user_answer = input("Is it even? (yes/no)")
+        correct_answer = "yes" if is_even(number) else "no"
+        if user_answer == correct_answer:
+            print("Correct!")
+        else:
+            print(f"{user_answer} is wrong answer :( The correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}.")
+            break
+    else:
+        print(f"Congratulations, {name}!")
+    
+if __name__ == "__main__":
+    main()
